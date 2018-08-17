581c18bee3eeedc08709d0f120a87716c00b782c
Veronica Flores <veronica.marie.flores@gmail.com>
2018-08-17 16:45:20 -0600

    push

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -5,6 +5,9 @@ import TodoService from "./todo-service.js";
 var todoService = new TodoService
 
 // Use this getTodos function as your callback for all other edits
+let todo = document.getElementById('todo-hidden')
+let weather = document.getElementById('weather-hidden')
+let quote = document.getElementById('quote-hidden')
 
 function draw(todos) {
 	//WHAT IS MY PURPOSE?
@@ -14,8 +17,8 @@ function draw(todos) {
 		const todo = todos[i];
 		template += `
 		<div id="todo" class="form-check col-xs-12 col-md-12">
-  			<input class="form-check-input" type="checkbox" >
-			<label class="form-check-label">${todo.description} <i class="fas fa-times" onclick="app.controllers.todoController.removeTodo('${todo._id}')"></i></label>
+		<input class="form-check-input" type="checkbox" >
+		<label class="form-check-label">${todo.description} <i class="fas fa-times" onclick="app.controllers.todoController.removeTodo('${todo._id}')"></i></label>
 		</div>
 		`
 	}
@@ -30,45 +33,51 @@ export default class TodoController {
 		this.getTodos()
 		// IF YOU WANT YOUR TODO LIST TO DRAW WHEN THE PAGE FIRST LOADS WHAT SHOULD YOU CALL HERE???
 	}
-	getTodos() {
+	open(id) {
+		id.hidden = false
+	}
+ 	close(id) {
+		id.hidden = true
+	}
+getTodos() {
 	//FYI DONT EDIT ME :)
 	todoService.getTodos(draw)
-	}
-	// You will need four methods
-	// getTodos should request your api/todos and give an array of todos to your callback fn
-	// addTodo takes in a todo and posts it to the server
-	// toggleTodoStatus takes in a todo marks its status as completed and puts it to the server
-	// removeTodo takes in a todoId and sends a delete request to the server
-	// **** HINT: Everytime you make a change to any todo don't forget to get the todo list again
+}
+// You will need four methods
+// getTodos should request your api/todos and give an array of todos to your callback fn
+// addTodo takes in a todo and posts it to the server
+// toggleTodoStatus takes in a todo marks its status as completed and puts it to the server
+// removeTodo takes in a todoId and sends a delete request to the server
+// **** HINT: Everytime you make a change to any todo don't forget to get the todo list again
 
-	addTodoFromForm(e) {
-		e.preventDefault() // <-- hey this time its a freebie don't forget this
-		// TAKE THE INFORMATION FORM THE FORM
-		let form = e.target
-		let todo = {
-			description: form.description.value,
-			// DONT FORGET TO BUILD YOUR TODO OBJECT
-		}
-
-		//PASSES THE NEW TODO TO YOUR SERVICE
-		//DON'T FORGET TO REDRAW THE SCREEN WITH THE NEW TODO
-		//YOU SHOULDN'T NEED TO CHANGE THIS
-		todoService.addTodo(todo, draw)
-		//^^^^^^^ EXAMPLE OF HOW TO GET YOUR TOODOS AFTER AN EDIT
-		form.reset()
+addTodoFromForm(e) {
+	e.preventDefault() // <-- hey this time its a freebie don't forget this
+	// TAKE THE INFORMATION FORM THE FORM
+	let form = e.target
+	let todo = {
+		description: form.description.value,
+		// DONT FORGET TO BUILD YOUR TODO OBJECT
 	}
 
-	toggleTodoStatus(todoId) {
-		// asks the service to edit the todo status
-		todoService.toggleTodoStatus(todoId, getTodos)
-		// YEP THATS IT FOR ME
-	}
+	//PASSES THE NEW TODO TO YOUR SERVICE
+	//DON'T FORGET TO REDRAW THE SCREEN WITH THE NEW TODO
+	//YOU SHOULDN'T NEED TO CHANGE THIS
+	todoService.addTodo(todo, draw)
+	//^^^^^^^ EXAMPLE OF HOW TO GET YOUR TOODOS AFTER AN EDIT
+	form.reset()
+}
 
-	removeTodo(todoId) {
-		// ask the service to run the remove todo with this id
-		todoService.removeTodo(todoId, draw)
-		// ^^^^ THIS LINE OF CODE PROBABLY LOOKS VERY SIMILAR TO THE toggleTodoStatus
-	}
+toggleTodoStatus(todoId) {
+	// asks the service to edit the todo status
+	todoService.toggleTodoStatus(todoId, getTodos)
+	// YEP THATS IT FOR ME
+}
+
+removeTodo(todoId) {
+	// ask the service to run the remove todo with this id
+	todoService.removeTodo(todoId, draw)
+	// ^^^^ THIS LINE OF CODE PROBABLY LOOKS VERY SIMILAR TO THE toggleTodoStatus
+}
 
 
 
